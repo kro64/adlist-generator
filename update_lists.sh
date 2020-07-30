7f34c7ca1b3fa73133bfc63ff605c1a45b3bc9f4
Karolis Birgela <karolisbirgela@gmail.com>
2020-07-30 22:57:04 +0300

    Feature to convert adlist into Unbound config

--- update_lists.sh
+++ update_lists.sh
@@ -2,28 +2,42 @@
 
 DIR=/opt/adblock_updater
 AD_LIST=$(curl -s https://v.firebog.net/hosts/lists.php?type=tick)
-CONF=$DIR/ad_blacklist.conf
+OUT=$DIR/ad_blacklist
 TEMP=$DIR/temp
+UNBOUND_CONF=$DIR/ad_blacklist.conf
+
+#--- Functions ---#
+
+convert_to_unbound() {
+while read LINE; do
+	echo local-zone: \"$LINE\" redirect >> $UNBOUND_CONF; echo local-data: \"$LINE A 127.0.0.1\" >> $UNBOUND_CONF;
+done < $OUT
+}
+
 
 # Make sure working directory exists
 mkdir -p $DIR
 
 # Clean the old blacklist
-echo > $CONF
+echo > $OUT
 
-# Update
+# Get latest ad domains (careful for rate limiting)
 for i in ${AD_LIST}; 
-	do curl -s $i >> $CONF
+	do curl -s $i >> $OUT
 done
 
 # Process the blacklist
 # Removing #
-sed -i '/^#/d' $CONF
+sed -i '/^#/d' $OUT
 # Removing IPs
-sed -i -r 's/([0-9]{1,3}\.){3}[0-9]{1,3}//g' $CONF
+sed -i -r 's/([0-9]{1,3}\.){3}[0-9]{1,3}//g' $OUT
 # Removing spaces etc.
-awk '{print $1}' $CONF > $TEMP; mv $TEMP $CONF
+awk '{print $1}' $OUT > $TEMP; mv $TEMP $OUT
 # Removing empty lines
-awk 'NF' $CONF > $TEMP; mv $TEMP $CONF
+awk 'NF' $OUT > $TEMP; mv $TEMP $OUT
 # Remove carriage return ^M
-sed -i 's/\r$//' $CONF
+sed -i 's/\r$//' $OUT
+
+
+echo > $UNBOUND_CONF
+convert_to_unbound
